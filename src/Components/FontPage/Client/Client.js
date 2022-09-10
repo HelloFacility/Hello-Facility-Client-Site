abd96ee13b52eb928b235bef1c00c48f16690b40
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-11 05:05:00 +0600

    footer added

--- src/Components/FontPage/Client/Client.js
+++ src/Components/FontPage/Client/Client.js
@@ -2,14 +2,14 @@ import React from "react";
 
 const Client = () => {
     return (
-        <div className="container mx-auto pt-16">
+        <div className="container px-20 pt-16">
             <div className="xl:w-full w-11/12 mx-auto">
             <h1 className="text-3xl font-bold xl:block hidden leading-tight text-sky-900">
             Our Valuable Client
                     </h1>
                 <p className="text-xl xl:text-left md:text-left lg:text-left md:w-full text-center xl:w-2/3 lg:w-2/3 w-11/12 mx-auto xl:mx-0 lg:mx-0 text-gray-600">You made it so simple. My new site is so much faster and easier to work with than my old site. I just choose the page, make the change and click save.</p>
             </div>
-            <div className="flex flex-wrap mt-10 xl:justify-between lg:justify-around md:justify-center justify-around xl:w-full lg:w-11/12 w-11/12 mx-auto xl:mx-0 lg:mx-auto">
+            <div className="flex flex-wrap gap-5 mt-10 xl:justify-between lg:justify-around md:justify-center justify-around xl:w-full lg:w-11/12 w-11/12 mx-auto xl:mx-0 lg:mx-auto">
                 <button className="bg-white shadow mb-2 xl:mb-0 lg:mb-0 rounded flex items-center justify-center h-24 w-40 cursor-pointer border border-transparent focus:border-gray-300 focus:shadow-md focus:outline-none">
                     <img src="https://cdn.tuk.dev/assets/adidas-gray.png" alt />
                 </button>
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -3,11 +3,11 @@ import BlogPage from '../FontPage/BlogPage/BlogPage';
 import Carousel from '../FontPage/Carousel/Carousel';
 import CleaningService from '../FontPage/CleaningService/CleaningService';
 import Client from '../FontPage/Client/Client';
-import Footer from '../FontPage/Footer/Footer';
 import Navbar from '../FontPage/Navbar/Navbar';
 import Post from '../FontPage/Post/Post';
 import RentalService from '../FontPage/RentalService/RentalService';
 import WhyChoose from '../FontPage/WhyChoose/WhyChoose';
+import Footer from '../Share/Footer/Footer';
 
 const Home = () => {
     return (
