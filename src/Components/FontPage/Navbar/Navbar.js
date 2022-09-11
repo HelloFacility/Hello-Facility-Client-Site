90a8f91137148ab47ae20bbea55334aabce12957
AlaminDolar <amin15-9898@diu.edu.bd>
2022-09-11 15:46:46 +0600

    simple navbar added

--- src/Components/FontPage/Navbar/Navbar.js
+++ src/Components/FontPage/Navbar/Navbar.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../../Images/ColorLogo.png';
+import './Navbar.css';
 
 const Navbar = () => {
     return (
         <div>
-            ssssssssssssssssssdddd
+            <div className='header'>
+                <img src={logo} alt="/" />
+                <nav>
+                    <Link to='/'>Home</Link>
+                    <Link to='/service'>Service</Link>
+                    <Link to='/client'>Client</Link>
+                    <Link to='/service'>Gallery</Link>
+                    <Link to='/blog'>Blog</Link>
+                    <Link to='/contactus'>Contact Us</Link>
+                </nav>
+            </div>
+
+
         </div>
     );
 };
