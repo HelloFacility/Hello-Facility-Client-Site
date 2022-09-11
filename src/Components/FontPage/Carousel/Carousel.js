6d1fd9cbcd4b8b34a9cc1dfa90a49c7cb33a7879
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-11 22:28:31 +0600

    Navbar added

--- src/Components/FontPage/Carousel/Carousel.js
+++ src/Components/FontPage/Carousel/Carousel.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Carousel = () => {
     return (
         <div>
-            Carousel
+            Carousel Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero porro incidunt eius necessitatibus qui voluptatum neque non, obcaecati, vero aliquid dolores, ipsa sed perspiciatis! Expedita maxime hic similique iure? Expedita, recusandae neque. At sequi nostrum, itaque beatae, quam ut accusamus veritatis nemo nobis quisquam, sit quaerat laudantium totam quod possimus. Quibusdam neque harum, omnis, dolor labore iste delectus, eius tempora quam voluptatem aut provident aliquam blanditiis obcaecati porro molestias aperiam architecto molestiae sint autem quis aliquid ullam? Porro et dolorum odit corporis cumque quis totam eius consectetur quod, tenetur aut. Fugiat, dolores. Amet dolore asperiores itaque. Tenetur est dolores esse distinctio laboriosam dolorum ab quo blanditiis fugiat quidem architecto, optio vitae! Eius ipsa, ab saepe veritatis debitis eveniet deserunt inventore asperiores, soluta ullam aperiam! Dolorum maiores qui quis laboriosam recusandae quidem suscipit tempora nostrum voluptatibus repudiandae mollitia impedit, tenetur quo autem? Maxime libero cupiditate recusandae sit voluptatibus voluptatem ipsam odio quas veritatis quod iusto itaque, sapiente, ullam voluptas animi, cum assumenda consectetur aperiam reiciendis vitae alias molestias totam. Saepe ea iste dolorum aspernatur excepturi placeat distinctio dolores sunt facilis laudantium molestias quia nihil iusto nisi odit quis corporis velit iure hic, quisquam dolorem vero. Perferendis velit porro blanditiis ut similique?
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['node_modules/daisyui/dist/**/*.js', 'node_modules/react-daisyui/dist/**/*.js'],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [require("daisyui")]
 }
