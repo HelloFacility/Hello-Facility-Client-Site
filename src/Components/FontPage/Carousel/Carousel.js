be3a3fefbb099a6324cb04ba62dbffe067b86952
AlaminDolar <amin15-9898@diu.edu.bd>
2022-09-12 10:19:19 +0600

    carosel position set

--- src/Components/FontPage/Carousel/Carousel.js
+++ src/Components/FontPage/Carousel/Carousel.js
@@ -3,10 +3,10 @@ import car from '../../../Images/car rental.avif';
 
 const Carousel = () => {
     return (
-        <div className='grid grid-cols-3 gap-4 bg-slate-300'>
-            <div className='mt-20 font-bold text-center text-black '>
+        <div className='grid items-center grid-cols-3 gap-4 bg-slate-300'>
+            <div className='font-bold text-center text-black '>
                 <h1>This is our best car for you,anytime you can rent this.It has 5000cc Engine and fully auto gear system and air condition.Its a family size car just awesome features.</h1>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+                <button class="bg-blue-500 hover:bg-blue-700 mt-3 text-white font-bold py-2 px-4 rounded-full">
                     Read More
                 </button>
             </div>
