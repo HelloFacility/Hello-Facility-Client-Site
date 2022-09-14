a66ba01dd8fa99314c2aa48d50be3b9b8dc1c2aa
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-15 00:26:42 +0600

    navber modify

--- src/Components/FontPage/Navbar/Navbar.js
+++ src/Components/FontPage/Navbar/Navbar.js
@@ -18,7 +18,7 @@ const Navbar = () => {
                                     Service
                                     <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" /></svg>
                                 </a>
-                                <ul className="text-black bg-white ">
+                                <ul className="text-black bg-white">
                                     <div >
                                         <div>
                                             <li>
@@ -77,7 +77,7 @@ const Navbar = () => {
                                 <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg>
                             </a>
                             <ul className="p-2 text-black bg-white">
-                                <div className='flex '>
+                                <div className='flex'>
                                     <div>
                                         <li>
                                             <Link className='font-bold' to='/cleaningService'>Cleaning Service</Link>
