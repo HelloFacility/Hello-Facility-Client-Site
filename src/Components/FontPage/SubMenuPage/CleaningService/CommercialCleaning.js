ca5de3c78987b38e561808fabe4adbf68a0cb999
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-15 04:14:49 +0600

    Additional Corporate service Added

--- src/Components/FontPage/SubMenuPage/CleaningService/CommercialCleaning.js
+++ src/Components/FontPage/SubMenuPage/CleaningService/CommercialCleaning.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import ServiceNavbar from '../../../Share/ServiceNavbar/ServiceNavbar';
+import CommercialCleaningService from '../../../../Images/CommercialCleaningServcie.jpg'
 
 const CommercialCleaning = () => {
     return (
         <div>
-            Commercial Cleaning
+            <ServiceNavbar />
+            <div className=' bg-sky-900  text-white flex justify-between items-center'>
+                <div className='pt-16 pl-32 pb-16'>
+                    <h1 className='font-bold text-3xl '>Commercial Cleaning</h1> <br />
+                    <p><small>(27331 ratings on 5 services)</small></p>
+                    <p>Trusted & Reliable AC Technicians</p>
+                    <p>Trusted & Reliable AC Technicians</p>
+                </div>
+                <div>
+                    <img className='pr-16' src={CommercialCleaningService} alt="" />
+                </div>
+            </div>
+            <div>
+
+            </div>
         </div>
     );
 };
