b84a72fd3eb8d4d9de335febdd2e4a502cb816f1
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-19 12:51:54 +0600

    order Form added

--- src/Components/FontPage/SubMenuPage/CleaningService/CommercialCleaning.js
+++ src/Components/FontPage/SubMenuPage/CleaningService/CommercialCleaning.js
@@ -25,8 +25,8 @@ import LaptopMacIcon from '@mui/icons-material/LaptopMac';
 import HotelIcon from '@mui/icons-material/Hotel';
 import RepeatIcon from '@mui/icons-material/Repeat';
 
-
-
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 import Rating from '@mui/material/Rating';
 import Stack from '@mui/material/Stack';
@@ -81,7 +81,58 @@ const CommercialCleaning = () => {
                     <h1 className='text-2xl font-bold text-sky-900 pb-5'>Plumbing & Sanitary Services</h1>
                     <form>
                         {/* Service Order Form */}
-                        {/* Popover MUI */}
+                        <Box
+                            component="form"
+                            sx={{
+                                '& .MuiTextField-root': { m: 1, width: '25ch' },
+                            }}
+                            noValidate
+                            autoComplete="off"
+                        >
+                            <div>
+                                <TextField
+                                    required
+                                    id="outlined-required"
+                                    label="Required"
+                                    defaultValue="Hello World"
+                                />
+                                <TextField
+                                    disabled
+                                    id="outlined-disabled"
+                                    label="Disabled"
+                                    defaultValue="Hello World"
+                                />
+                                <TextField
+                                    id="outlined-password-input"
+                                    label="Password"
+                                    type="password"
+                                    autoComplete="current-password"
+                                />
+                                <TextField
+                                    id="outlined-read-only-input"
+                                    label="Read Only"
+                                    defaultValue="Hello World"
+                                    InputProps={{
+                                        readOnly: true,
+                                    }}
+                                />
+                                <TextField
+                                    id="outlined-number"
+                                    label="Number"
+                                    type="number"
+                                    InputLabelProps={{
+                                        shrink: true,
+                                    }}
+                                />
+                                <TextField id="outlined-search" label="Search field" type="search" />
+                                <TextField
+                                    id="outlined-helperText"
+                                    label="Helper text"
+                                    defaultValue="Default Value"
+                                    helperText="Some important text"
+                                />
+                            </div>
+                        </Box>
                     </form>
                 </div>
             </div>
