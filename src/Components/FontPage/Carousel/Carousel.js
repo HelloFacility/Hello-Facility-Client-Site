5eb6f8ed64bb925b932cfeed32695158173770c1
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-23 07:27:39 +0600

    carousel update

--- src/Components/FontPage/Carousel/Carousel.js
+++ src/Components/FontPage/Carousel/Carousel.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import car from '../../../Images/SubmenuService/SubMenuTopImg.jpg';
 
 
@@ -10,27 +11,15 @@ const Carousel = () => {
             <div className='h-96 mt-1' style={{
                 backgroundImage: `url(${car})`
             }}>
-                <div className="text-center">
-                    <h1 className="pt-10 text-5xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-5xl">
-                        <span className="block xl:inline md:pl-2 text-white">Data to enrich your</span>{' '} <br />
-                        <span className="block  xl:inline md:pl-2 text-white">online business</span>
-                    </h1>
-                    <p className='text-center mt-3 text-xl text-white'>Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. 
-                    <br />Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
-                    <p className="text-center mt-3 text-base text-gray-500 sm:mx-auto sm:mt-5 sm:max-w-xl sm:text-lg md:mt-5 md:text-xl lg:mx-0 md:pl-2">
-                        
-                    </p>
-                    <div className="mt-5 sm:mt-8 sm:flex justify-center">
-                        <div className="rounded-md shadow">
-                            <a
-                                href="#"
-                                className="flex items-center justify-center w-full px-8 py-3 text-base font-medium text-white bg-sky-900 border border-transparent rounded-md hover:bg-sky-700 md:py-4 md:px-10 md:text-lg"
-                            >
-                                Contact Us
-                            </a>
-                        </div>
-                        <br />
-                    </div>
+
+                <div>
+                    <h1 className='lg:text-4xl md:text-3xl text-2xl font-bold text-white text-center pt-10'>Data to enrich your</h1>
+                    <h1 className='lg:text-4xl md:text-3xl text-2xl font-bold text-white text-center'>online business</h1>
+                    <p className='text-xl text-white text-center mt-10'>Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                        <br />Elit sunt amet fugiat veniam occaecat fugiat aliqua.</p>
+                </div>
+                <div className='flex justify-center pt-10'>
+                    <Link className='font-bold shadow-sm bg-gray-100 px-4 py-3 rounded-md' to='/'>Learn More</Link>
                 </div>
             </div>
         </div>
