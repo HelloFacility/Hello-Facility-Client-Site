08b66692a95825d96dc26a09892c3ed66f27077e
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-25 16:38:58 +0600

    authentication added

--- src/Components/Share/Navbar/Navbar.js
+++ src/Components/Share/Navbar/Navbar.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import logo from '../../../Images/ColorLogo.png';
+import auth from '../../firebase.init';
+import { signOut } from 'firebase/auth';
+import { Avatar, Button, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip, Typography } from '@mui/material';
+import { IoIosArrowDown } from 'react-icons/io';
+import { Logout, PersonAdd, Settings } from '@mui/icons-material';
+import { Box } from '@mui/system';
+import { MdManageAccounts } from 'react-icons/md';
+
 
 const Navbar = () => {
+    const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
+    const [user] = useAuthState(auth)
+    console.log(user);
+    const handleSignOut = () => {
+        signOut(auth)
+    }
     return (
         <div className='shadow-md'>
             <div className='lg:mx-16'>
@@ -127,7 +151,84 @@ const Navbar = () => {
                         </ul>
                     </div>
                     <div className="navbar-end">
-                        <Link to='/signIn' className="font-bold">Login</Link>
+                        {
+                            user ?
+                                <div>
+                                    <React.Fragment>
+                                        
+                                        <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
+                                        <p>{user?.displayName}</p>
+                                            <Tooltip title="Account settings">
+                                                <IconButton
+                                                    onClick={handleClick}
+                                                    size="small"
+                                                    sx={{ ml: 2 }}
+                                                    aria-controls={open ? 'account-menu' : undefined}
+                                                    aria-haspopup="true"
+                                                    aria-expanded={open ? 'true' : undefined}
+                                                >
+                                                    <Avatar sx={{ width: 32, height: 32 }}><img src={user?.photoURL} alt="" /></Avatar>
+                                                </IconButton>
+                                            </Tooltip>
+                                        </Box>
+                                        <Menu
+                                            anchorEl={anchorEl}
+                                            id="account-menu"
+                                            open={open}
+                                            onClose={handleClose}
+                                            onClick={handleClose}
+                                            PaperProps={{
+                                                elevation: 0,
+                                                sx: {
+                                                    overflow: 'visible',
+                                                    filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                                                    mt: 1.5,
+                                                    '& .MuiAvatar-root': {
+                                                        width: 32,
+                                                        height: 32,
+                                                        ml: -0.5,
+                                                        mr: 1,
+                                                    },
+                                                    '&:before': {
+                                                        content: '""',
+                                                        display: 'block',
+                                                        position: 'absolute',
+                                                        top: 0,
+                                                        right: 14,
+                                                        width: 10,
+                                                        height: 10,
+                                                        bgcolor: 'background.paper',
+                                                        transform: 'translateY(-50%) rotate(45deg)',
+                                                        zIndex: 0,
+                                                    },
+                                                },
+                                            }}
+                                            transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                                            anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                                        >
+                                            <MenuItem>
+                                                <Avatar src={user?.photoURL} /> My Profile
+                                            </MenuItem>
+                                            <Divider />
+                                            <MenuItem>
+                                                <ListItemIcon>
+                                                    <Settings fontSize="small" />
+                                                </ListItemIcon>
+                                                My Account
+                                            </MenuItem>
+                                            <MenuItem>
+                                                <ListItemIcon>
+                                                    <Logout fontSize="small" />
+                                                </ListItemIcon>
+                                                <Link to='/' onClick={handleSignOut}>Sign Out</Link>
+                                            </MenuItem>
+                                        </Menu>
+                                    </React.Fragment>
+                                </div>
+
+                                :
+                                <Link to='/signIn'><span>Login</span></Link>
+                        }
                     </div>
                 </div>
             </div>
---
+++ src/Components/firebase.init.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAuhP2BkNziVlZNwXCSZNAndx4L4mioIKM",
+  authDomain: "hello-facility-bd.firebaseapp.com",
+  projectId: "hello-facility-bd",
+  storageBucket: "hello-facility-bd.appspot.com",
+  messagingSenderId: "846365497495",
+  appId: "1:846365497495:web:f5af987bba2e2a75f1f1d2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth (app)
+export default auth;
