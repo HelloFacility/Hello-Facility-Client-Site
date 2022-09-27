f51ac65e2a859ae06babb2270a768214928af9c3
alhabib911 <abdullahalhabib100@gmail.com>
2022-09-27 19:51:23 +0600

    Edit Address added

--- src/Components/Dashboard/CustomersDashboard/AddressBook/AddressBook.js
+++ src/Components/Dashboard/CustomersDashboard/AddressBook/AddressBook.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import EditAddress from './EditAddress';
 
 const AddressBook = () => {
+    const [newAddressBook, setNewAddressBook] = useState([]);
+    const [addressEdit, setAddressEdit] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/newAddress')
+            .then((res) => res.json())
+            .then((data) => setNewAddressBook(data.reverse()));
+    }, [newAddressBook]);
+
+    // console.log(newAddressBook);
     return (
         <div className='mx-5 mt-5'>
             <div>
@@ -11,16 +22,19 @@ const AddressBook = () => {
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
                                 <th scope="col" class="py-3 px-6">
-                                    Product name
+                                    Contact Person
                                 </th>
                                 <th scope="col" class="py-3 px-6">
-                                    Color
+                                    Phone Number
                                 </th>
                                 <th scope="col" class="py-3 px-6">
-                                    Category
+                                    City
                                 </th>
                                 <th scope="col" class="py-3 px-6">
-                                    Price
+                                    Area
+                                </th>
+                                <th scope="col" class="py-3 px-6">
+                                    Address
                                 </th>
                                 <th scope="col" class="py-3 px-6">
                                     <span class="sr-only">Edit</span>
@@ -28,31 +42,60 @@ const AddressBook = () => {
                             </tr>
                         </thead>
                         <tbody>
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    Apple MacBook Pro 17"
-                                </th>
-                                <td class="py-4 px-6">
-                                    Sliver
-                                </td>
-                                <td class="py-4 px-6">
-                                    Laptop
-                                </td>
-                                <td class="py-4 px-6">
-                                    $2999
-                                </td>
-                                <td class="py-4 px-6 text-right">
-                                    <Link className='font-medium text-blue-600' to='/dashboard/editAddress'>Edit</Link>
-                                </td>
-                            </tr>
+                            {newAddressBook.map((newAddress) => (
+
+                                <tr
+                                    key={newAddress._id}
+                                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                    <th
+                                        scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                        {newAddress?.name}
+                                    </th>
+                                    <td class="py-4 px-6">
+                                        {newAddress?.phone}
+                                    </td>
+                                    <td class="py-4 px-6">
+                                        {newAddress?.city}
+                                    </td>
+                                    <td class="py-4 px-6">
+                                        {newAddress?.area}
+                                    </td>
+                                    <td class="py-4 px-6">
+                                        {newAddress?.shortAddress}
+                                        <br />
+                                        <small>{newAddress?.fullAddress}</small>
+                                    </td>
+
+
+
+                                    <td className='py-4 px-4 text-right'>
+                                        <label
+                                            for="edit-address-modal"
+                                            onClick={() =>
+                                                setAddressEdit(
+                                                    newAddress
+                                                )
+                                            }
+                                        >
+                                            <span className="font-medium text-blue-600 cursor-pointer">
+                                            Edit
+                                            </span>
+                                        </label>
+                                    </td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
 
             </div>
             <div className='flex lg:justify-end justify-center mr-5'>
-            <Link className='bg-sky-900 py-3 px-20 text-white font-semibold mt-24 cursor-pointer' to='/dashboard/addNewAddress'>+ ADD NEW ADDRESS</Link>
+                <Link className='bg-sky-900 py-3 px-20 text-white font-semibold mt-24 cursor-pointer' to='/dashboard/addNewAddress'>+ ADD NEW ADDRESS</Link>
             </div>
+
+            <EditAddress
+                addressEdit={addressEdit}
+            ></EditAddress>
         </div>
     );
 };
--- src/Components/Dashboard/CustomersDashboard/AddressBook/EditAddress.js
+++ src/Components/Dashboard/CustomersDashboard/AddressBook/EditAddress.js
@@ -1,9 +1,218 @@
+import { FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 import React from 'react';
+import { AiFillSave } from 'react-icons/ai';
+import Swal from 'sweetalert2';
 
-const EditAddress = () => {
+const EditAddress = ({addressEdit}) => {
+    const { _id } =
+    addressEdit;
+    const [city, setCity] = React.useState('');
+    const [area, setArea] = React.useState('');
+
+    const handleCity = (event) => {
+        setCity(event.target.value);
+    };
+
+    const handleArea = (event) => {
+        setArea(event.target.value);
+    };
+    const handleEditAddress = e => {
+        e.preventDefault();
+        const name = e.target.name.value;
+        const phone = e.target.phone.value;
+        const shortAddress = e.target.shortAddress.value;
+        const fullAddress = e.target.fullAddress.value;
+
+        const EditAddress = {
+            name,
+            phone,
+            city,
+            area,
+            shortAddress,
+            fullAddress,
+        };
+
+        console.log(EditAddress);
+
+        const url = `http://localhost:5000/newAddress/${_id}`
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json',
+                // authorization: `Bearer ${localStorage.getItem('accessToken')}`
+            },
+            body: JSON.stringify(EditAddress)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+                Swal.fire(
+                    'Good job!',
+                    'Profile Updated!',
+                    'success'
+                  )
+                e.target.reset()
+            })
+    }
+    //     const url = `http://localhost:5000/editAddress/${_id}`;
+    //     fetch(url, {
+    //         method: "PUT",
+    //         headers: {
+    //             "content-type": "application/json",
+    //         },
+    //         body: JSON.stringify(handleEditAddress),
+    //     })
+    //         .then((res) => res.json())
+    //         .then((data) => {
+    //             console.log("success", data);
+    //             e.target.reset();
+    //         });
+    // }
     return (
         <div>
-            Edit Address
+            <div>
+                <input
+                    type="checkbox"
+                    id="edit-address-modal"
+                    className="modal-toggle"
+                />
+                <div className="modal modal-bottom sm:modal-middle">
+                    <div className="modal-box">
+                        <label
+                            for="edit-address-modal"
+                            className="btn btn-sm btn-circle absolute right-2 top-2"
+                        >
+                            ✕
+                        </label>
+                        <div>
+                            <h2 className='font-bold text-xl mb-5 mt-4'>Address Book</h2>
+                            <form onSubmit={handleEditAddress}>
+                                <div className='grid lg:grid-cols-2 grid-cols-1 gap-3 mt-5'>
+                                    <TextField
+                                        name="name"
+                                        id="standard-number"
+                                        label="Contact Person"
+                                        type="text"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        variant="standard"
+                                    />
+
+                                    <TextField
+                                        name="phone"
+                                        id="standard-number"
+                                        label="Phone Number"
+                                        type="number"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        variant="standard"
+                                    />
+                                </div>
+
+                                <h2 className="mt-5 font-medium text-blue-500">
+                                    Address
+                                </h2>
+                                <div className='grid lg:grid-cols-2 grid-cols-1 gap-3 mt-5'>
+                                    <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                                        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-standard-label"
+                                            id="demo-simple-select-standard"
+                                            value={city}
+                                            label="City"
+                                            onChange={handleCity}
+                                        >
+                                            <MenuItem value={'Dhaka North'}>Dhaka North</MenuItem>
+                                            <MenuItem value={'Dhaka South'}>Dhaka South</MenuItem>
+                                        </Select>
+                                    </FormControl>
+
+                                    <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+                                        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-standard-label"
+                                            id="demo-simple-select-standard"
+                                            value={area}
+                                            label="Area"
+                                            onChange={handleArea}
+                                        >
+                                            <MenuItem value={'Cantonment area'}>Cantonment area</MenuItem>
+                                            <MenuItem value={'Banani'}>Banani</MenuItem>
+                                            <MenuItem value={'Gulshan'}>Gulshan</MenuItem>
+                                            <MenuItem value={'Mohakhali'}>Mohakhali</MenuItem>
+                                            <MenuItem value={'Bashundhara'}>Bashundhara</MenuItem>
+                                            <MenuItem value={'Banasree'}>Banasree</MenuItem>
+                                            <MenuItem value={'Banani'}>Banani</MenuItem>
+                                            <MenuItem value={'Baridhara'}>Baridhara</MenuItem>
+                                            <MenuItem value={'Uttarkhan'}>Uttarkhan</MenuItem>
+                                            <MenuItem value={'Banani'}>Banani</MenuItem>
+                                            <MenuItem value={'Dakshinkhan'}>Dakshinkhan</MenuItem>
+                                            <MenuItem value={'Bawnia'}>Bawnia</MenuItem>
+                                            <MenuItem value={'Khilkhet'}>Khilkhet</MenuItem>
+                                            <MenuItem value={'Tejgaon'}>Tejgaon</MenuItem>
+                                            <MenuItem value={'Farmgate'}>Farmgate</MenuItem>
+                                            <MenuItem value={'Mohammadpur'}>Mohammadpur</MenuItem>
+                                            <MenuItem value={'Rampura'}>Rampura</MenuItem>
+                                            <MenuItem value={'Badda'}>Badda</MenuItem>
+                                            <MenuItem value={'Satarkul'}>Satarkul</MenuItem>
+                                            <MenuItem value={'Beraid'}>Beraid</MenuItem>
+                                            <MenuItem value={'Khilgaon'}>Khilgaon</MenuItem>
+                                            <MenuItem value={'Vatara'}>Vatara</MenuItem>
+                                            <MenuItem value={'Gabtali'}>Gabtali</MenuItem>
+                                            <MenuItem value={'Hazaribagh'}>Hazaribagh</MenuItem>
+                                            <MenuItem value={'Dhanmondi'}>Dhanmondi</MenuItem>
+                                            <MenuItem value={'Ramna'}>Ramna</MenuItem>
+                                            <MenuItem value={'Motijheel'}>Motijheel</MenuItem>
+                                            <MenuItem value={'Sabujbagh'}>Sabujbagh</MenuItem>
+                                            <MenuItem value={'Lalbagh'}>Lalbagh</MenuItem>
+                                            <MenuItem value={'Kamalapur'}>Kamalapur</MenuItem>
+                                            <MenuItem value={'Kamrangirchar'}>Kamrangirchar</MenuItem>
+                                            <MenuItem value={'Sadarghat'}>Sadarghat</MenuItem>
+                                            <MenuItem value={'Wari'}>Wari</MenuItem>
+                                            <MenuItem value={'Kotwali'}>Kotwali</MenuItem>
+                                            <MenuItem value={'Sutrapur'}>Sutrapur</MenuItem>
+                                            <MenuItem value={'Jurain'}>Jurain</MenuItem>
+                                            <MenuItem value={'Dania'}>Dania</MenuItem>
+                                            <MenuItem value={'Demra'}>Demra</MenuItem>
+                                            <MenuItem value={'Shyampur'}>Shyampur</MenuItem>
+                                            <MenuItem value={'Nimtoli'}>Nimtoli</MenuItem>
+                                            <MenuItem value={'Shahbagh'}>Shahbagh</MenuItem>
+                                            <MenuItem value={'Paltan'}>Paltan</MenuItem>
+                                        </Select>
+                                    </FormControl>
+
+                                    <TextField
+                                        name="shortAddress"
+                                        id="standard-number"
+                                        label="House No / Floor / Street"
+                                        type="text"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        variant="standard"
+                                    />
+
+                                    <TextField
+                                        name="fullAddress"
+                                        id="standard-number"
+                                        label="Address"
+                                        type="text"
+                                        InputLabelProps={{
+                                            shrink: true,
+                                        }}
+                                        variant="standard"
+                                    />
+                                </div>
+                                <div className='flex justify-center mr-5'>
+                                    <input className='bg-sky-900 py-3 px-20 text-white font-semibold mt-10 cursor-pointer' type="submit" value="SAVE CHANGE" />
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
