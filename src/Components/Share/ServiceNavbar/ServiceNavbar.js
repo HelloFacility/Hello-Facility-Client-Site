996c203dfa45f27a86c16fa8edef7ef7ed8c1cf8
alhabib911 <abdullahalhabib100@gmail.com>
2022-12-09 03:34:38 +0600

    code clean

--- src/Components/Share/ServiceNavbar/ServiceNavbar.js
+++ src/Components/Share/ServiceNavbar/ServiceNavbar.js
@@ -1,32 +1,8 @@
 import React from 'react';
-import { useAuthState } from 'react-firebase-hooks/auth';
 import { Link } from 'react-router-dom';
 import logo from '../../../Images/ColorLogo.png';
-import auth from '../../firebase.init';
-import { signOut } from 'firebase/auth';
-import { Avatar, Box, Divider, IconButton, ListItemIcon, Menu, MenuItem, Tooltip } from '@mui/material';
-import { Logout, Settings } from '@mui/icons-material';
-import { MdDashboard } from 'react-icons/md';
-
-
 
 const ServiceNavbar = () => {
-    const [user] = useAuthState(auth)
-    console.log(user);
-    const handleSignOut = () => {
-        signOut(auth)
-    }
-    const [anchorEl, setAnchorEl] = React.useState(null);
-
-    const open = Boolean(anchorEl);
-    const handleClick = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-        setAnchorEl(null);
-    };
-
-
     return (
         <div className='flex justify-between items-center px-16 py-3 bg-slate-50 shadow-sm'>
             <div className='w-1/5'>
